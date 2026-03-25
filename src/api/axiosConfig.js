5711706e83b1b5c8964b58761cc22a5d7412fc45
D32SSV <131158288+D32SSV@users.noreply.github.com>
2026-03-25 17:31:31 +0530

    ensure mandatory https in prod

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,10 +1,16 @@
 import axios from "axios";
 
+const API_URL = import.meta.env.VITE_BACKEND_API
+
+
+if (import.meta.env.NODE_ENV === 'production' && !API_URL.startsWith('https://')) {
+  throw new Error("Insecure API URL detected. HTTPS is required in production.");
+}
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_API,
+  baseURL: API_URL,
   withCredentials: true, // MUST be true to send/receive cookies
 });
-
 
 api.interceptors.request.use(
   (config) => {
@@ -21,7 +27,6 @@ api.interceptors.request.use(
     if (csrfToken) {
       config.headers["X-XSRF-TOKEN"] = csrfToken;
     }
-
     return config;
   },
   (error) => {
