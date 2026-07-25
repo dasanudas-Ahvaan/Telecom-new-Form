912a476bc9292a9c653ddd0a85bfdbdc7802049c
D32SSV <131158288+D32SSV@users.noreply.github.com>
2026-07-25 16:08:16 +0530

    refactor: update Vite configuration to load environment variables dynamically

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  esbuild: {
-    drop: env.NODE_ENV === "development" ? [] : ["console", "debugger"],
-  },
-  build: {
-    sourcemap: false,
-  },
-});
+export default ({ mode }) => {
+  // Load env 
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+    esbuild: {
+      drop: env.NODE_ENV === 'development' ? [] : ['console', 'debugger'],
+    },
+    build: {
+      sourcemap: false,
+    },
+  });
+};
