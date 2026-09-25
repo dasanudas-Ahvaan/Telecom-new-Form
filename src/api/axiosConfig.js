58ef43d5f0f7a43aa8a43e41e834256cd5ebd413
D32SSV <131158288+D32SSV@users.noreply.github.com>
2026-09-25 17:16:41 +0530

    handle prod and dev modes

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_BACKEND_API;
 
 if (
-  import.meta.env.NODE_ENV === "production" &&
+  import.meta.env.PROD &&
   !API_URL.startsWith("https://")
 ) {
   throw new Error(
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,14 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     plugins: [react(), tailwindcss()],
     base: "/register/",
     esbuild: {
-      drop: env.NODE_ENV === "development" ? [] : ["console", "debugger"],
+      drop: mode === "development" ? [] : ["console", "debugger"],
     },
     build: {
       sourcemap: false,
